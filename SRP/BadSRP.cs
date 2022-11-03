0ce3a03536ef2bf266ae750e51ad3df93bbc236f
Carlos Castro <carloscastrogames@gmail.com>
2022-11-03 09:25:56 -0300

    first commit

---
+++ SRP/BadSRP.cs
@@ -0,0 +1,16 @@
+namespace solid.SRP;
+public class Pedido
+{
+    public void CalcularTotal() {/*...*/}
+    public void ObterItens() {/*...*/}
+    public void ObterTotalItens() {/*...*/}
+    public void AddItem(String item) {/*...*/}
+    public void DeleteItem(String item) {/*...*/}
+
+    public void ShowPedido() {/*...*/}
+
+    public void Load() {/*...*/}
+    public void Save() {/*...*/}
+    public void Update() {/*...*/}
+    public void Delete() {/*...*/}
+}
